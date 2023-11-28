55ac015caca946bc1d6599278886256496f8e910
Frank Maximus <frank@healingjourneyman.com>
2023-11-28 19:19:09 +0100

    first commit

---
+++ src/main/java/com/example/delijn/DelijnApplication.java
@@ -0,0 +1,13 @@
+package com.example.delijn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelijnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelijnApplication.class, args);
+    }
+
+}
